a81315a3f1574eafdc956227b46964416f0dd312
Boudaya Yassin <boudayayacin007@gmail.com>
2024-03-08 00:21:02 +0100

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import { dividerClasses } from "@mui/material";
 import Image from "next/image";
 import Menu from "@/components/client/menu";
 import MainGridHome from "@/components/homepage";
+import Footer from "@/components/client/footer";
 export default function Home() {
   return (
  <>
  <MainGridHome />
+ <Footer />
  </>
   );
 }
